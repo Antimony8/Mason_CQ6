d4e255aef99ad328cae89bd27c2d7a390f166e7e
Antimony8 <97124775+Antimony8@users.noreply.github.com>
2023-10-23 12:30:17 -0400

    Create Plus_Minus.java

---
+++ Plus_Minus.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+        int n = arr.size();
+        int positive = 0;
+        int negative = 0;
+        int zero = 0;
+
+
+        for (int num : arr) {
+            if (num > 0) {
+                positive++;
+            } else if (num < 0) {
+                negative++;
+            } else {
+                zero++;
+            }
+        }
+
+
+        // Calculate proportions
+        double positiveRatio = (double) positive / n;
+        double negativeRatio = (double) negative / n;
+        double zeroRatio = (double) zero / n;
+
+
+        // Print with 6 decimal places
+        System.out.printf("%.6f%n", positiveRatio);
+        System.out.printf("%.6f%n", negativeRatio);
+        System.out.printf("%.6f%n", zeroRatio);
+    }
+
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
